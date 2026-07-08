1c6643fc6f5c1c081f99e6db190715301a7eba04
Nguyễn Tuấn Hưng <thwng08092007@gmail.com>
2026-07-08 13:08:06 +0700

    Update via Git-Auto

---
+++ user_service.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from models import StudentModel
+
+def delete_student_service(db: Session, student_id: int):
+    student = db.query(StudentModel).filter(StudentModel.id == student_id).first()
+    
+    if not student:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Học viên không tồn tại trong hệ thống"
+        )
+    
+    student_data = {
+        "id": student.id,
+        "full_name": student.full_name,
+        "email": student.email
+    }
+    
+    db.delete(student)
+    db.commit()
+    
+    return student_data
